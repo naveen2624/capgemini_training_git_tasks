1a79d809a6bd4cec624c1c21a006325f4f60b6b1
naveen2624 <naveenudai26@gmail.com>
2025-12-15 15:26:18 +0530

    Add Calculator with add and subtract methods

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -11,14 +11,14 @@ class Program
         int b = Convert.ToInt16(Console.ReadLine());
         switch (choice)
         {
-            case 1: Console.WriteLine("{0} + {1} = {2}", a, b, a + b);
+            case 1: Console.WriteLine("Addition\n{0} + {1} = {2}", a, b, a + b);
                 break;
-            case 2: Console.WriteLine("{0} - {1} = {2}", a, b, a - b);
+            case 2: Console.WriteLine("Subtraction\n{0} - {1} = {2}", a, b, a - b);
                 break;
             default: Console.WriteLine("Invalid Choice");
                 break;
         }
 
 
-    }
+    }   
 }
