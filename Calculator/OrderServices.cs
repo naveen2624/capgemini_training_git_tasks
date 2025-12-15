4354586f3e06750093bb44fc1d40c00a0bf80430
naveen2624 <naveenudai26@gmail.com>
2025-12-15 15:56:50 +0530

    new commit

---
+++ Calculator/OrderServices.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public class OrderService
+    {
+        public void PlaceOrder()
+        {
+            Console.WriteLine("Order placed");
+        }
+    }
+
+}
